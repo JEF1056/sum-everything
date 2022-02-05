398ef54e46d64da5c212e17c5a9d65402a9fca85
Jess Fan <jessefan2002@gmail.com>
2022-02-05 12:26:23 -0800

    Add tqdm

--- data/gen-wiki.py
+++ data/gen-wiki.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import gdown
 import zipfile
+from tqdm import tqdm
 from src.helpers import clean
 
 DATA_URL = "https://drive.google.com/uc?export=download&id=1BnPhEZISDwuWIEzLMQeud-HBA4BSUUor"
@@ -17,18 +18,18 @@ if not os.path.exists(IN):
         for file in fz.namelist():
             fz.extract(file, ".")
 
-title_file = open('wikiHow/titles.txt', mode="r", encoding="utf-8")
+title_file = open('wikiHow/titles.txt', mode="r", encoding="utf-8").read().split("\n")
 path = "wikiHow/articles"
-train_data = open("../datasets/wikiHow.train", mode="w", encoding="utf-8")
-val_data = open("../datasets/wikiHow.validation", mode="w", encoding="utf-8")
+os.makedirs("../datasets/wiki", exist_ok=True)
+train_data = open("../datasets/wiki/wikiHow.train", mode="w", encoding="utf-8")
+val_data = open("../datasets/wiki/wikiHow.validation", mode="w", encoding="utf-8")
 outputs = [train_data, val_data]
 overall_count = [0, 0]
 if len(sys.argv) == 1: np.random.seed(2022)
 else: np.random.seed(int(sys.argv[1]))
 error_count = 0
 
-title = title_file.readline().replace("\n", "")
-while title:
+for title in tqdm(title_file):
     file_path = path + "/" + title + ".txt"
     try:
         f = open(file_path, mode='r', encoding="utf-8")
@@ -45,10 +46,7 @@ while title:
         error_count += 1
         # print(e)
 
-    title = title_file.readline().replace("\n", "")
-
 print("overall_count: [train, val]=", overall_count)
 print("file name encoding error: ", error_count)
 train_data.close()
 val_data.close()
-title_file.close()
